a1aba43512c28b00ff30136757d348583e9c932b
joergviola <joerg@joergviola.de>
2015-03-18 23:09:41 +0100

    first webgl light...

--- output.php
+++ output.php
@@ -1,3 +1,75 @@
 
+<div id="stage"></div>
 
-TEST
+<script>
+	width = 300;
+	height = 200;
+	
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize( width, height );
+	renderer.setClearColor( 0xffffff );
+	document.getElementById("stage").appendChild( renderer.domElement );	 
+
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshPhongMaterial( { 
+		color: 0xcccc00, 
+		specular: 0x009900, 
+		shininess: 30, 
+		shading: THREE.FlatShading	
+	} );
+	var cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+	
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+	directionalLight.position.set( 0, 1, 0 );
+	scene.add( directionalLight );
+		
+	var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	scene.add( light );
+	
+	camera.position.z = 20;
+
+	controls = new THREE.OrbitControls( camera );
+	controls.damping = 0.2;
+	controls.addEventListener( 'change', render );
+
+	
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+		console.log( item, loaded, total );
+	};
+	var onProgress = function ( xhr ) {
+		if ( xhr.lengthComputable ) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+	var onError = function ( xhr ) {
+	};
+	var texture = new THREE.Texture();
+	var loader = new THREE.OBJLoader( manager );
+	loader.load( 
+		'<?php echo plugins_url('models/ferrari/ferrari-f1-race-car.obj', __FILE__) ?>', 
+		function ( object ) {
+			object.traverse( function ( child ) {
+				if ( child instanceof THREE.Mesh ) {
+					child.material.color.setHex( 0xffaa66 );
+				}
+			} 
+		);
+		object.position.z = - 100;
+		scene.add( object );
+	}, onProgress, onError );
+
+	
+	function render() {
+		requestAnimationFrame( render );
+
+		renderer.render( scene, camera );
+	}
+	render();		
+</script>
+
--- wordpress-3d.php
+++ wordpress-3d.php
@@ -15,6 +15,7 @@ class WP_3D {
 	// Constructor
 	public function __construct() {
 		add_shortcode( '3D', array('WP_3D', 'shortcode_3d') );
+		add_action( 'wp_enqueue_scripts', array('WP_3D', 'add_scripts') );
 	}
 	
 	// Activate the plugin
@@ -30,8 +31,20 @@ class WP_3D {
 		// Do nothing so far ;)
 	}
 	
+	public function add_scripts() {
+		wp_register_script('threejs', plugins_url('js/three.min.js', __FILE__), array(),'1.1', false);
+		wp_enqueue_script('threejs');
+		wp_register_script('collada-loader', plugins_url('js/loaders/ColladaLoader.js', __FILE__), array(),'1.1', false);
+		wp_enqueue_script('collada-loader');
+		wp_register_script('obj-loader', plugins_url('js/loaders/OBJLoader.js', __FILE__), array(),'1.1', false);
+		wp_enqueue_script('obj-loader');
+		wp_register_script('orbital', plugins_url('js/controls/OrbitControls.js', __FILE__), array(),'1.1', false);
+		wp_enqueue_script('orbital');
+	}
+	
+	
 	// The 3D shortcode
-	function shortcode_3d($atts, $content = null) {
+	public function shortcode_3d($atts, $content = null) {
 		ob_start();
 		require('output.php');
 		return ob_get_clean();
